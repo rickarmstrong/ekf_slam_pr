Fix G_t jacobian matrix.

src/ekf_slam/ekf.py
@@ -51,8 +51,19 @@ def get_vel_cmd(R=np.array([0., 0.])):
     return u, u_noisy
 
 
-def G(u_t, mu, delta_t=DELTA_T):
-    """Return the Jacobian of the motion model function g()."""
+def G_t_full(u_t, mu, delta_t=DELTA_T):
+    """Return the jacobian of the motion model, padded-out to the size of
+    the state covariance. This lets us transform only the motion portion
+    of the covariance, leaving the landmark portions untouched."""
+    G_x_t = G_t_x(u_t, mu, delta_t)
+    return np.block([
+        [G_x_t, np.zeros((POSE_DIMS, LM_DIMS * N_LANDMARKS))],
+        [np.zeros((LM_DIMS * N_LANDMARKS, POSE_DIMS)), np.eye((LM_DIMS * N_LANDMARKS))]
+    ])
+
+
+def G_t_x(u_t, mu, delta_t=DELTA_T):
+    """Return the 3x3 Jacobian of the motion model function g()."""
     v_t = u_t[0]
     omega_t = u_t[1]
     theta = mu[2]